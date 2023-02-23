cbd6c7d0ff6fde0be5d9bcf7089ef2048c4b4954
Marcelo Lemes <marceloaflemes@gmail.com>
2023-02-23 14:10:20 -0300

    feat: incluindo base para o banco de dados

---
+++ ShoplistAPI/ScriptsDB/shoplist-structure.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE shoplist (
+    id INTEGER IDENTITY PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT
+)
+
+CREATE TABLE product (
+    id INTEGER IDENTITY PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    brand VARCHAR(255),
+    description TEXT,
+    number INTEGER NOT NULL,
+    shoplistId INTEGER,
+    FOREIGN KEY (shoplistId)
+     REFERENCES shoplist (id)
+)
+
+
